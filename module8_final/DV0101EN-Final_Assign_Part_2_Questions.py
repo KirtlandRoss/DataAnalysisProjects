520d17d40ab28940513f19a9845b00fc355aba5e
kirtlandross <kirtlandross@gmail.com>
2023-12-11 17:09:15 -0500

    Updated capstone week 1

--- module8_final/DV0101EN-Final_Assign_Part_2_Questions.py
+++ module8_final/DV0101EN-Final_Assign_Part_2_Questions.py
@@ -34,7 +34,7 @@ year_list = [i for i in range(1980, 2024, 1)]
 app.layout = html.Div([
     #TASK 2.1 Add title to the dashboard
     html.H1("Automobile Sales Statistics Dashboard", 
-    style={'textAlign': 'left', 'color': '#503D36','font-size': 24}), #May include style for title
+    style={'textAlign': 'left', 'color': '#503D36','font-size': 24}),#May include style for title
     html.Div([#TASK 2.2: Add two dropdown menus
         html.Label("Select Statistics:"),
         dcc.Dropdown(
@@ -51,6 +51,7 @@ app.layout = html.Div([
             placeholder = 'Select Year',
             value=1980
         )),
+    
     html.Div([#TASK 2.3: Add a division for output display
     html.Div(id='output_container', className='chart-grid', style={'display':'flex'})])
 ])
